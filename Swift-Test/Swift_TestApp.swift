c38bbdf1ceb46d36d447115e3138ce2f5fb895a6
Masami Yamate <yamate.inquiry@mail.yamatte.net>
2021-06-15 23:32:40 +0900

    init

---
+++ Swift-Test/Swift_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TestApp.swift
+//  Swift-Test
+//
+//  Created by Masami on 2021/06/15.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
